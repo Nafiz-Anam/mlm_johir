d8555296a7edfeeff13b3a49086a4ad5acd98194
Nafiz_Anam <nafiz.anam002@gmail.com>
2023-05-31 22:38:28 +0600

    bug fixing

--- src/middleware/web/auth.js
+++ src/middleware/web/auth.js
@@ -16,7 +16,8 @@ const verifyToken = async (req, res, next) => {
     console.log("---------------", tokenFromDB, token);
     if (tokenFromDB != token || tokenFromDB == false) {
       console.log("=================tokenFromDB==========================");
-      return res.status(401).json({ status: false });
+      // return res.status(401).json({ status: false });
+      return res.status(401).send("token expired");
     }
     req.user = decoded;
     return next();
